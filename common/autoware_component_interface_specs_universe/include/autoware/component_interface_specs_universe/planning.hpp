543273ad37b1050225b6faf69d93d02abdb2ad2f
Ryohsuke Mitsudome <ryohsuke.mitsudome@tier4.jp>
2025-06-19 03:54:32 +0900

    Revert "feat!: replace tier4_planning_msgs service with autoware_planning_msgs (#10827)"
    This reverts commit 60ac065e41bb98efb0a6a01e4b20526cfe52ddf3.

--- common/autoware_component_interface_specs_universe/include/autoware/component_interface_specs_universe/planning.hpp
+++ common/autoware_component_interface_specs_universe/include/autoware/component_interface_specs_universe/planning.hpp
@@ -18,37 +18,38 @@
 #include <rclcpp/qos.hpp>
 
 #include <autoware_planning_msgs/msg/lanelet_route.hpp>
-#include <autoware_planning_msgs/msg/route_state.hpp>
 #include <autoware_planning_msgs/msg/trajectory.hpp>
-#include <autoware_planning_msgs/srv/clear_route.hpp>
-#include <autoware_planning_msgs/srv/set_lanelet_route.hpp>
-#include <autoware_planning_msgs/srv/set_waypoint_route.hpp>
+#include <tier4_planning_msgs/msg/route_state.hpp>
+#include <tier4_planning_msgs/srv/clear_route.hpp>
+#include <tier4_planning_msgs/srv/set_lanelet_route.hpp>
+#include <tier4_planning_msgs/srv/set_waypoint_route.hpp>
 
 namespace autoware::component_interface_specs_universe::planning
 {
 
 struct SetLaneletRoute
 {
-  using Service = autoware_planning_msgs::srv::SetLaneletRoute;
-  static constexpr char name[] = "/planning/set_lanelet_route";
+  using Service = tier4_planning_msgs::srv::SetLaneletRoute;
+  static constexpr char name[] = "/planning/mission_planning/route_selector/main/set_lanelet_route";
 };
 
 struct SetWaypointRoute
 {
-  using Service = autoware_planning_msgs::srv::SetWaypointRoute;
-  static constexpr char name[] = "/planning/set_waypoint_route";
+  using Service = tier4_planning_msgs::srv::SetWaypointRoute;
+  static constexpr char name[] =
+    "/planning/mission_planning/route_selector/main/set_waypoint_route";
 };
 
 struct ClearRoute
 {
-  using Service = autoware_planning_msgs::srv::ClearRoute;
-  static constexpr char name[] = "/planning/clear_route";
+  using Service = tier4_planning_msgs::srv::ClearRoute;
+  static constexpr char name[] = "/planning/mission_planning/route_selector/main/clear_route";
 };
 
 struct RouteState
 {
-  using Message = autoware_planning_msgs::msg::RouteState;
-  static constexpr char name[] = "/planning/route_state";
+  using Message = tier4_planning_msgs::msg::RouteState;
+  static constexpr char name[] = "/planning/mission_planning/route_selector/main/state";
   static constexpr size_t depth = 1;
   static constexpr auto reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
   static constexpr auto durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
@@ -57,7 +58,7 @@ struct RouteState
 struct LaneletRoute
 {
   using Message = autoware_planning_msgs::msg::LaneletRoute;
-  static constexpr char name[] = "/planning/route";
+  static constexpr char name[] = "/planning/mission_planning/route_selector/main/route";
   static constexpr size_t depth = 1;
   static constexpr auto reliability = RMW_QOS_POLICY_RELIABILITY_RELIABLE;
   static constexpr auto durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
